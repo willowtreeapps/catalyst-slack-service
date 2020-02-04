274d15dd10d2eded648068c756015922ed99c9cb
Edlyn Haynes <edlyn.haynes@willowtreeapps.com>
2020-02-04 12:57:38 -0500

    Help Controller

--- app/org/catalyst/slackservice/util/PayloadHelper.java
+++ app/org/catalyst/slackservice/util/PayloadHelper.java
@@ -1,15 +1,27 @@
 package org.catalyst.slackservice.util;
 
-import java.util.Arrays;
-import java.util.Optional;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PayloadHelper {
 
-    public static Optional<String> getFirstArrayValue(String[] array) {
-        if (array == null) {
-            return Optional.empty();
-        }
+    public final static String UTF8 = "UTF-8";
+    public final static Charset CHARSET_UTF8 = Charset.forName(UTF8);
 
-        return Arrays.asList(array).stream().findFirst();
+    public static Map<String, String> getFormUrlEncodedRequestBody(String request) {
+        List<NameValuePair> parsedRequest = URLEncodedUtils.parse(request, CHARSET_UTF8);
+
+        return parsedRequest.stream().collect(
+                Collectors.toMap(NameValuePair::getName, NameValuePair::getValue));
+    }
+
+    public static String getMapValue(Map<String, String> map, String key) {
+        var value = map.get(key);
+        return value == null ? "" : value;
     }
 }
